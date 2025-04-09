7bf2ff26aeeea55d95c59101cd094f948b396076
Waissi <adr.adm@protonmail.com>
2025-04-09 07:53:39 +0200

    update vim config files

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,9 +7,9 @@ map("n", "<leader>w", ":bdelete<CR>", opts)
 map("n", "<leader>e", ":Lexplore<CR>", opts)
 map("n", "<leader>s", ":w<CR>", opts)
 map("n", "<leader>q", ":q<CR>", opts)
-map("n", "<leader>p", ":find ", {})
-map("n", "<leader>f", [[:execute 'find ' .. expand('<cword>') .. '.' .. &filetype<CR>]], {})
-map("n", "<leader>F", [[:execute 'vimgrep /' .. expand('<cword>') .. '/' '**/*.' .. &filetype<CR>]], {})
+map("n", "<leader>p", ":find ", {noremap = true})
+map("n", "<leader>f", [[:execute 'find ' .. expand('<cword>') .. '.' .. &filetype<CR>]], opts)
+map("n", "<leader>F", [[:execute 'vimgrep /' .. expand('<cword>') .. '/' '**/*.' .. &filetype<CR>]], opts)
 map("n", "<leader>S", ":Startify<CR>", opts)
 map("n", "<S-l>", ":bnext<CR>", opts)
 map("n", "<S-h>", ":bprevious<CR>", opts)
@@ -18,10 +18,9 @@ map("n", "<S-k>", "<C-b><CR>", opts)
 map("n", "<Up>", ":cprevious<CR>", opts)
 map("n", "<Down>", ":cnext<CR>", opts)
 map("n", "<F6>", ":!scripts/run.sh<CR>", opts)
-vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, opts)
 
 vim.opt.path:append("**")
-vim.opt.title = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
@@ -29,49 +28,50 @@ vim.opt.autoindent = true
 vim.opt.wildmenu = true
 vim.opt.number = true
 vim.opt.wildignore:append("*.md")
+vim.opt.mouse = ""
 vim.g.netrw_winsize = 25
 vim.g.netrw_liststyle = 3
 vim.g.netrw_banner = 0
 
-vim.cmd([[
-call plug#begin('~/.config/nvim/plugged')
+local Plug = vim.fn['plug#']
+vim.call('plug#begin', '~/.config/nvim/plugged')
 Plug 'folke/tokyonight.nvim'
 Plug 'mhinz/vim-startify'
 Plug 'neovim/nvim-lspconfig'
-call plug#end()
-]])
+vim.call('plug#end')
+
 vim.cmd("colorscheme tokyonight-night")
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#b0b0b0" })
 vim.g.startify_lists = {
-  { type = 'sessions', header = {'   Saved Sessions'} },
+    { type = 'sessions', header = {'   Saved Sessions'} },
 }
 
 vim.api.nvim_set_keymap('n', 'R', [[:lua FindAndReplace()<CR>]], { noremap = true, silent = true })
 
 function FindAndReplace()
-  local old = vim.fn.expand('<cword>')
-  local new = vim.fn.input("Replace " .. old .. " with: ")
-  if new ~= "" then
-    vim.cmd('%s/' .. old .. '/' .. new .. '/gc')
-  end
+    local old = vim.fn.expand('<cword>')
+    local new = vim.fn.input("Replace " .. old .. " with: ")
+    if new ~= "" then
+        vim.cmd('%s/' .. old .. '/' .. new .. '/gc')
+    end
 end
 
 require'lspconfig'.lua_ls.setup {
   on_init = function(client)
       client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        version = 'LuaJIT'
-      },
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME,
-          "${3rd}/love2d/library"
-        }
-      }
-    })
+          runtime = {
+              version = 'LuaJIT'
+          },
+          workspace = {
+              checkThirdParty = false,
+              library = {
+                  vim.env.VIMRUNTIME,
+                  "${3rd}/love2d/library"
+             }
+          }
+     })
   end,
   settings = {
-    Lua = {}
+      Lua = {}
   }
 }
